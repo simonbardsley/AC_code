66a84c68ae0c4d2c37f050ea60b88ecfbc904cd7
simonbardsley <paraflight2011@gmail.com>
2017-04-17 23:25:09 +1200

    Changed for loop. Working on Error handeling next.

--- logitDir_win10.py
+++ logitDir_win10.py
@@ -11,27 +11,50 @@
 import os, re, time, errno, sys
 from os import listdir, path, chmod
 
-#Logit list
+#Logit list variable
 logitClass = {0: 'DP' , 1: 'SA' , 2: 'DS' , 3: 'PS' , 4: 'IN' , 5: 'MR' , 6: 'LOGIMA'}
 
-#Variables
-logitType = input("Choose a logit type number: 0:'DP', 1:'SA', 2:'DS', 3:'PS', 4:'IN', 5:'MR', 6:'LOGIMA' ")
-print(logitType)
-changeNum = input("Provide the logit change number (e.g. C12345): ")
+#Logit name variables
+logitType = input("Choose a logit type number: 0:'DP', 1:'SA', 2:'DS', 3:'PS', 4:'IN', 5:'MR', 6:'LOGIMA' "[0:1]
+#print(logitType)
+changeNum = input("Provide the logit change number (e.g. C12345): ")[0:6]
 shortDesc = input("Write a short description (<15 char): ")[0:15]
-customerName = input("Add the customers name? ")
+customerName = input("Add the customers name? ")[0:15]
 os.chdir("C:\\Users\\simonbardsley\\Downloads")
 print(os.getcwd())
 
 
+# Check if the folder exists, if not create it either as a general logit or a data provision request and exit if statement
 if logitType == 2:
     companyName = input("What is the company name? ")
-    logitName = changeNum + " - " + shortDesc + " - " + customerName + " - " + companyName + " - " + logitClass[logitType]
+    logitName = str(changeNum + " - " + shortDesc + " - " + customerName + " - " + companyName + " - " + logitClass[logitType])
     path = os.path.join("C:\\Users\\simonbardsley\\Downloads\\", logitName)
     if not os.path.exists(path):
         os.mkdir(path)
-        # logitName = changeNum + " - " + shortDesc + " - " + customerName + " - " + companyName + " - " + logitClass[logitType]
         print("A logit folder was created as: " + logitName)
+        time.sleep(2)
+        #os.chmod(path, mode=777)
+        os.chdir(path)
+        print("###################################")
+        print("Creating subfolders")
+        time.sleep(2)
+        print("Documents...")
+        time.sleep(2)
+        os.mkdir("1 Documents")
+        print("Inputs...")
+        time.sleep(2)
+        os.mkdir("2 Inputs")
+        print("Data...")
+        time.sleep(2)
+        os.mkdir("3 Data")
+        print("Workspaces...")
+        time.sleep(2)
+        os.mkdir("4 Workspaces")
+        print("Outputs...")
+        os.mkdir("5 Outputs." )
+        time.sleep(2)
+        print("###################################")
+        print("Listing Logit directory contents")
     else:
         print("Directory already exists!")
         sys.exit
@@ -40,36 +63,34 @@ else:
     path = os.path.join("C:\\Users\\simonbardsley\\Downloads\\", logitName)
     if not os.path.exists(path):
         os.mkdir(path)
+        time.sleep(2)
+        #os.chmod(path, mode=777)
+        os.chdir(path)
+        print("###################################")
+        print("Creating subfolders")
+        time.sleep(2)
+        print("Documents...")
+        time.sleep(2)
+        os.mkdir("1 Documents")
+        print("Inputs...")
+        time.sleep(2)
+        os.mkdir("2 Inputs")
+        print("Data...")
+        time.sleep(2)
+        os.mkdir("3 Data")
+        print("Workspaces...")
+        time.sleep(2)
+        os.mkdir("4 Workspaces")
+        print("Outputs...")
+        os.mkdir("5 Outputs." )
+        time.sleep(2)
+        print("###################################")
+        print("Listing Logit directory contents")
         print("A logit folder was created as: " + logitName)
     else:
-        print(Directory already exists!")
+        print("Directory already exists!")
         sys.exit
 
-
-time.sleep(2)
-#os.chmod(path, mode=777)
-os.chdir(path)
-print("###################################")
-print("Creating subfolders")
-time.sleep(2)
-print("Documents...")
-time.sleep(2)
-os.mkdir("1 Documents")
-print("Inputs...")
-time.sleep(2)
-os.mkdir("2 Inputs")
-print("Data...")
-time.sleep(2)
-os.mkdir("3 Data")
-print("Workspaces...")
-time.sleep(2)
-os.mkdir("4 Workspaces")
-print("Outputs...")
-os.mkdir("5 Outputs." )
-time.sleep(2)
-print("###################################")
-print("Listing Logit directory contents")
-time.sleep(2)
 
 list = os.listdir(path)
 
